e708f5cebfe530f92a8618c6fbcba69ff68aa6b1
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-06-08 10:14:50 +0200

    foocus on textInput added

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,24 +1,38 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./style.css";
 
 const Form = ({ addNewTask }) => {
   const [newTaskContent, setNewTaskContent] = useState("");
+  const textFocus = useRef();
+
+  const setTextFocus = () => {
+    textFocus.current.focus();
+  }
 
   const onFormSubmit = (event) => {
     event.preventDefault();
-    addNewTask(newTaskContent.trim());
+    if (newTaskContent.trim() !== "") {
+      addNewTask(newTaskContent);
+    }
     setNewTaskContent("");
   };
+
 
   return (
     <form className="form" onSubmit={onFormSubmit}>
       <input
         value={newTaskContent}
+        ref={textFocus}
         className="form__text" type="text"
         placeholder="Co jest do zrobienia?"
         onChange={({ target }) => setNewTaskContent(target.value)}
       />
-      <button className="form__button" >Dodaj zadanie</button>
+      <button
+        className="form__button"
+        onClick={setTextFocus}
+      >
+        Dodaj zadanie
+      </button>
     </form>
   );
 }
