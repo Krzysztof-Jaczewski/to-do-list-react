568ed50c9e5995d093c69be8662992e71116ac1e
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-06-08 06:47:44 +0200

    index.js formated

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -2,23 +2,23 @@ import "./style.css"
 
 const Buttons = ({ tasks, hideDone }) => (
 
-    <div className="buttons">
-       {tasks.length > 0 && (
+  <div className="buttons">
+    {tasks.length > 0 && (
       <>
-      <button
-        className="buttons__button "
-      >
-        {hideDone ? "Pokaż" : "Ukryj"} ukończone
+        <button
+          className="buttons__button "
+        >
+          {hideDone ? "Pokaż" : "Ukryj"} ukończone
       </button>
-      <button
-        className="buttons__button"
-        disabled={tasks.every(({ done }) => done)}
-      >
-        Ukończ wszystkie
+        <button
+          className="buttons__button"
+          disabled={tasks.every(({ done }) => done)}
+        >
+          Ukończ wszystkie
       </button>
       </>
-      )}
-    </div>
+    )}
+  </div>
 );
 
 
