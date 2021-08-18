bc3f7d79a8ff183c00cbbfca46f49049998aabe2
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-08-18 11:53:37 +0200

    accept rename action bug fixed

--- src/features/tasks/TaskRename/Index.js
+++ src/features/tasks/TaskRename/Index.js
@@ -3,7 +3,6 @@ import { TextRenameForm } from "./styled";
 import { Button } from "../TasksList/styled";
 import { useDispatch } from "react-redux";
 import { cancelRenameTask, acceptRenameTask } from "../TasksSlice";
-import { nanoid } from "@reduxjs/toolkit";
 
 export const TaskRename = ({ task }) => {
   const [newTaskContent, setNewTaskContent] = useState("");
@@ -25,16 +24,18 @@ export const TaskRename = ({ task }) => {
       <Button
         type="submit"
         title="Zatwierdź zmianę"
-        onClick={() =>
-          dispatch(
-            acceptRenameTask({
-              content: newTaskContent,
-              done: false,
-              rename: false,
-              id: nanoid(),
-            })
-          )
-        }
+        onClick={() => {
+          if (newTaskContent === "") dispatch(cancelRenameTask(task.id));
+          else
+            dispatch(
+              acceptRenameTask({
+                content: newTaskContent,
+                done: false,
+                rename: false,
+                id: task.id,
+              })
+            );
+        }}
       >
         ✔
       </Button>
