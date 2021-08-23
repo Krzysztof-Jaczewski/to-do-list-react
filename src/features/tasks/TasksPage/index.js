a8004899a2eebf238f096509a3ace6fdc605f384
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-08-23 20:36:20 +0200

    import fixed in TaskSPage

--- src/features/tasks/TasksPage/index.js
+++ src/features/tasks/TasksPage/index.js
@@ -3,9 +3,9 @@ import { Section } from "../../../common/Section";
 import { Buttons } from "./Buttons";
 import { Header } from "../../../common/Header";
 import { Main } from "../../../common/Main";
-import { ExampleButton } from "./ExampleButton/ExampleButton";
 import { TasksList } from "./TasksList";
 import { Search } from "./Search";
+import { ExampleButton } from "./ExampleButton";
 
 export const TasksPage = () => {
   return (
