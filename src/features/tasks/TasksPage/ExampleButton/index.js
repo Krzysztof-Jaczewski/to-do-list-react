02456fef8a33ff793d9e68a7018a1359b28442c5
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-08-23 20:35:24 +0200

    exampleButon renamed

--- src/features/tasks/TasksPage/ExampleButton/index.js
+++ src/features/tasks/TasksPage/ExampleButton/index.js
@@ -1,14 +1,16 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { AdditionalButtons, Button } from "../../../../common/Button";
-import { axiosExampleTasks } from "../../tasksSlice";
+import { axiosExampleTasks, selectLoading } from "../../tasksSlice";
+import { Loading } from "../../../../common/Loading";
 
 export const ExampleButton = () => {
   const dispatch = useDispatch();
+  const loading = useSelector(selectLoading);
 
   return (
     <AdditionalButtons>
       <Button onClick={() => dispatch(axiosExampleTasks())}>
-        pokaż przykładowe zadania
+        {loading ? <Loading /> : "pokaż przykładowe zadania"}
       </Button>
     </AdditionalButtons>
   );
