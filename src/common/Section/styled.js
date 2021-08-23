5ec2377e1cd1b7a20fdffdec61413c4204294c24
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-08-23 17:36:12 +0200

    removed unnesesery exports from section

--- src/common/Section/styled.js
+++ src/common/Section/styled.js
@@ -27,33 +27,3 @@ export const SectionBody = styled.div`
   padding: 20px;
   background-color: white;
 `;
-
-export const AdditionalButtons = styled.div`
-  display: grid;
-  grid-template-columns: auto auto;
-  grid-gap: 20px;
-
-  @media (max-width: ${({ theme }) => theme.size.mobile}) {
-    grid-template-columns: 1fr;
-  }
-`;
-
-export const StyledButton = styled.button`
-  border: none;
-  background-color: white;
-  color: hsl(180, 100%, 25%);
-  transition: color 0.5s linear;
-
-  &:disabled {
-    color: #ddd;
-    pointer-events: none;
-  }
-
-  &:hover {
-    filter: ${({ theme }) => theme.filter.hover};
-  }
-
-  &:active {
-    filter: ${({ theme }) => theme.filter.active};
-  }
-`;
