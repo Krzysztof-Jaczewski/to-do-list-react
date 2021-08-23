6e22243ed2d761c78aa34c288c79317f69c14dad
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-08-23 17:33:23 +0200

    created Hashrouter

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
-import { HashRouter, NavLink, Redirect, Route, Switch } from "react-router-dom";
-import AuthorPage from "./features/author/AuthorPage";
-import { Tasks } from "./features/tasks/Tasks";
+import { HashRouter, Redirect, Route, Switch } from "react-router-dom";
+import { AuthorPage } from "./features/author/AuthorPage";
+import { TasksPage } from "./features/tasks/TasksPage";
+import { TaskPage } from "./features/tasks/TaskPage";
+import { Navigation } from "./features/Navigation";
 
 const App = () => (
   <HashRouter>
-    <nav>
-      <ul>
-        <li>
-          <NavLink to="/zadania">Zadania</NavLink>
-        </li>
-        <li>
-          <NavLink to="/autor">O autorze</NavLink>
-        </li>
-      </ul>
-      <Switch>
-        <Route path="/zadania">
-          <Tasks />
-        </Route>
-        <Route path="/autor">
-          <AuthorPage />
-        </Route>
-        <Route path="/">
-          <Redirect to="/zadania" />
-        </Route>
-      </Switch>
-    </nav>
+    <Navigation />
+    <Switch>
+      <Route path="/zadania/:id">
+        <TaskPage />
+      </Route>
+      <Route path="/zadania">
+        <TasksPage />
+      </Route>
+      <Route path="/autor">
+        <AuthorPage />
+      </Route>
+      <Route path="/">
+        <Redirect to="/zadania" />
+      </Route>
+    </Switch>
   </HashRouter>
 );
 
