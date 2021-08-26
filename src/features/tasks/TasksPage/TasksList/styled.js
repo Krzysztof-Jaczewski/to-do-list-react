83117f9234f969777ada4a0a3f6273f4bb29c08f
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-08-26 19:37:15 +0200

    added hover on task

--- src/features/tasks/TasksPage/TasksList/styled.js
+++ src/features/tasks/TasksPage/TasksList/styled.js
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom";
 import styled, { css } from "styled-components";
 
 export const List = styled.ul`
-  background-color: white;
   list-style: none;
   padding: 0;
 `;
@@ -22,17 +21,45 @@ export const Item = styled.span`
     `}
 `;
 
-export const ListText = styled.span`
+export const StyledLink = styled(Link)`
+  padding: 6px;
+  text-decoration: none;
+  color: black;
+  width: 100%;
+  position: relative;
   word-break: break-word;
-
+  background-color: transparent;
+  cursor: pointer;
   ${({ done }) =>
     done &&
     css`
       text-decoration: line-through;
     `}
-`;
-
-export const StyledLink = styled(Link)`
-  text-decoration: none;
-  color: black;
+  &:hover {
+    z-index: 1;
+    color: white;
+  }
+  &::before {
+    content: "";
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    transform: scaleX(0);
+    transform-origin: bottom right;
+    background-image: linear-gradient(
+      to right,
+      hsl(120, 100%, 25%),
+      hsl(200, 95%, 40%)
+    );
+    transition: transform 0.5s ease;
+    height: 1.9em;
+  }
+  &:hover::before {
+    transform: scaleX(1);
+    transform-origin: bottom left;
+    z-index: -1;
+  }
 `;
